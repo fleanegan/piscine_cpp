4b8ab26b89bd4de2689ee3dc316d0daf1f3904c4
te <testsetestsetestsaegsr@trash-mail.com>
2022-05-13 11:36:17 +0200

    feat(d1, ex05): Harl is complaining

---
+++ d1/src/ex05/Harl.h
@@ -0,0 +1,41 @@
+//
+// Created by fleanegan on 12.05.22.
+//
+
+#ifndef EX03_WEAPON_H
+#define EX03_WEAPON_H
+
+#include <string>
+#include <iostream>
+#include <utility>
+
+class Harl;
+
+typedef  void (Harl::*HarlMemFn)(void) const;
+
+typedef struct s {
+	std::string name;
+	HarlMemFn function;
+} funDict;
+
+class Harl {
+private:
+	void debug(void ) const;
+	void info( void )  const;
+	void warning( void )  const;
+	void error( void )  const;
+	funDict complainArray[4] = {{"debug", &Harl::debug},\
+							{"info", &Harl::info},\
+							{"warning", &Harl::warning},\
+							{"error", &Harl::error}};
+public:
+	Harl(const Harl& other);
+	Harl & operator = (const Harl& other);
+	~Harl();
+	Harl();
+
+	void	complain(std::string level);
+};
+
+
+#endif //EX03_WEAPON_H
