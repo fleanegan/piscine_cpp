2df117c12d0df33d074ca919e9c92dd87e5ded40
te <testsetestsetestsaegsr@trash-mail.com>
2022-05-13 10:24:16 +0200

    fix(d1, ex04): fixed error with whitespace

--- d1/src/ex04/main.cpp
+++ d1/src/ex04/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 
 int initFiles(int argc, char *const *argv, std::ifstream &inFile, std::ofstream &outFile);
-std::string createSeparator(std::ifstream &inFile);
-std::string replace(const std::string &word, const char *replacee, const char *replacer);
+
+bool
+isCurrentWordReplaceeCandidate(std::ifstream &inFile, const std::string &current_word, const std::string &replacee);
+
+void doReplacement(std::ofstream &outFile, std::string &current_word, const std::string &replacer);
+
+void writeLeftoversToOutfile(const std::string &replacee, const std::string &replacer, std::ofstream &outFile,
+							 std::string &current_word);
+
+std::string &appendNextCharIfEmptyReplacementCandidate(std::ifstream &inFile, std::string &replacementCandidate);
+
+void writeReplacerIfMatch(const std::string &replacee, const std::string &replacer, std::ofstream &outFile,
+						  std::string &current_word);
+
+void buildReplacementCandidate(const std::string &replacee, std::ifstream &inFile, std::string &current_word);
 
 #ifndef TESTING
 #include "fstream"
@@ -10,42 +23,74 @@ std::string replace(const std::string &word, const char *replacee, const char *r
 int main(int argc, char *argv[]) {
 	std::ifstream	inFile;
 	std::ofstream	outFile;
-	std::string		current_word;
+	std::string		replacementCandidate;
+	std::string		replacee;
+	std::string		replacer;
 
 	if (initFiles(argc, argv, inFile, outFile))
 		return (-1);
-	outFile << createSeparator(inFile);
-	while (inFile >> current_word) {
-		outFile << replace(current_word, argv[2], argv[3]);
-		outFile << createSeparator(inFile);
+	replacee = argv[2];
+	replacer = argv[3];
+	while (inFile.peek() != EOF) {
+		buildReplacementCandidate(replacee, inFile, replacementCandidate);
+		writeReplacerIfMatch(replacee, replacer, outFile, replacementCandidate);
+		replacementCandidate = appendNextCharIfEmptyReplacementCandidate(inFile, replacementCandidate);
+		writeLeftoversToOutfile(replacee, replacer, outFile, replacementCandidate);
 	}
 	inFile.close();
 	outFile.close();
 	return (0);
 }
 
-std::string replace(const std::string &word, const char *replacee, const char *replacer) {
-	if (word == replacee)
-		return (replacer);
+void	buildReplacementCandidate(\
+		const std::string &replacee, std::ifstream &inFile, std::string &current_word) {
+	while (isCurrentWordReplaceeCandidate(inFile, current_word, replacee)\
+			&& current_word != replacee)
+		current_word += inFile.get();
+}
+
+void	writeReplacerIfMatch(\
+		const std::string &replacee, const std::string &replacer, std::ofstream &outFile, std::string &current_word) {
+	if (current_word == replacee && replacee.length() != 0)
+		doReplacement(outFile, current_word, replacer);
+}
+
+std::string	&appendNextCharIfEmptyReplacementCandidate(\
+			std::ifstream &inFile, std::string &replacementCandidate) {
+	if (inFile.peek() != EOF && replacementCandidate.empty())
+		replacementCandidate = inFile.get();
+	return replacementCandidate;
+}
+
+void	writeLeftoversToOutfile(\
+		const std::string &replacee, const std::string &replacer, std::ofstream &outFile, std::string &current_word) {
+	if (current_word != replacee)
+		outFile << current_word;
 	else
-		return (word);
+		doReplacement(outFile, current_word, replacer);
+	current_word.clear();
 }
 
-std::string createSeparator(std::ifstream &inFile) {
-	std::string	result;
-	while (inFile.peek() != EOF && (inFile.peek() == '\n' || std::isspace(inFile.peek())))
-		result += inFile.get();
-	return (result);
+void	doReplacement(\
+		std::ofstream &outFile, std::string &current_word, const std::string &replacer) {
+	current_word.clear();
+	outFile << replacer;
 }
 
-int initFiles(int argc, char *const *argv, std::ifstream &inFile, std::ofstream &outFile) {
+bool	isCurrentWordReplaceeCandidate(\
+		std::ifstream &inFile, const std::string &current_word, const std::string &replacee) {
+	return inFile.peek() != EOF && replacee.find(current_word) != std::string::npos;
+}
+
+int initFiles(\
+	int argc, char *const *argv, std::ifstream &inFile, std::ofstream &outFile) {
 	std::string		inFileName;
 	std::string		outFileName;
 
-	inFileName = argv[1];
-	outFileName = inFileName + ".replace";
 	if (argc != 4)
 		return (1);
+	inFileName = argv[1];
+	outFileName = inFileName + ".replace";
 	inFile.open(inFileName);
 	outFile.open(outFileName);
 	if (inFile.is_open() == false)
