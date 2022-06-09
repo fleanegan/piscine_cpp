5d63040e2c95ed4a825a376ce2c4fcb0d0097516
Frederik SCHLUTER <fschlute@e1r1p8.42.fr>
2022-06-09 13:25:08 +0200

    fix(d8, ex00): test with different containers

--- d8/ex00/src/main.cpp
+++ d8/ex00/src/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <list>
 #include "easyfind.hpp"
 
 #ifndef TESTING
 template <typename T>
-int printSearchResult(std::vector<T> &container, const typename std::vector<T>::iterator &result);
+int printSearchResult(std::list<T> &container, const typename std::list<T>::iterator &result);
 template <typename T>
 int printSearchResult(const std::vector<T> &container, const typename std::vector<T>::iterator &result);
 
@@ -23,16 +24,16 @@ int main() {
 		std::cout << "found " << *unsuccessfulSearch << std::endl;
 
 	static const float arrString[] = {0, 2, 3, 4.324};
-	std::vector<float> containerString(arrString, arrString + sizeof(arrString) / sizeof(arrString[0]) );
-	std::vector<float>::iterator successfulSearchString = easyfind<std::vector<float> >(containerString, 3);
+	std::list<float> containerString(arrString, arrString + sizeof(arrString) / sizeof(arrString[0]) );
+	std::list<float>::iterator successfulSearchString = easyfind<std::list<float> >(containerString, 3);
 	printSearchResult(containerString, successfulSearchString);
-	std::vector<float>::iterator unsuccessfulSearchString = easyfind(containerString, 200);
+	std::list<float>::iterator unsuccessfulSearchString = easyfind(containerString, 200);
 	printSearchResult(containerString, unsuccessfulSearchString);
 	return 0;
 }
 
 template <typename T>
-int printSearchResult(std::vector<T> &container, const typename std::vector<T>::iterator &result){
+int printSearchResult(std::list<T> &container, const typename std::list<T>::iterator &result){
 	if (result == container.end())
 		std::cout << "did not find the element" << std::endl;
 	else
